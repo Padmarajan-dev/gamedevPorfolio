c22ee8149629bfeee43fc7ffc71b5a2de72e83c9
Rajudev <rajussp31121997@gmail.com>
2022-02-26 14:45:34 +0530

    assets added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+document.head.appendChild(jQueryScript);
+
+var popper = document.createElement('script');  
+jQueryScript.setAttribute('src','https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js');
+document.head.appendChild(popper);
+
